d0a6f657edb494d22b4256af12d8526614309532
Sasch <sascha.zierke@outlook.com>
2021-08-18 18:00:45 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import psycopg2
+import anvil.server
+
+anvil.server.connect("RF76OLM6BUKQZFEZXOSGMMNR-YU2HXYOZY4XXCSYD")
+
+conn = psycopg2.connect(
+    host='localhost',
+    user='postgres',
+    password='passwordpsql'
+)
+
+conn.set_session(autocommit=True)
+
+cur = conn.cursor()
+
+
+@anvil.server.callable
+def get_items(db):
+    cur.execute(f'SELECT * FROM {db}')
+    items = cur.fetchall()
+    return [
+        {'id': item[0], 'invest': float(item[1])}
+        for item in items
+    ]
+
+
+@anvil.server.callable
+def get_last_2_items(db):
+    cur.execute(f'''
+    SELECT id, invest from {db}
+    ORDER BY id DESC 
+    LIMIT 2''')
+    items = cur.fetchall()
+    return [float(item[1])
+            for item in items]
+
+
+@anvil.server.callable
+def get_last_item(db):
+    cur.execute(f''' 
+    SELECT id, invest from {db}
+    ORDER BY id DESC 
+    LIMIT 1
+    ''') #ohne limit cur.fetchone() gibt erstes item
+    items = cur.fetchall()
+    return [float(item[1])
+            for item in items]
+
+
+
+anvil.server.wait_forever()
