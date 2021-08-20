ccb39dbfebaec2d8d0aeadf2177ccd679e21214d
CBSaschaZierke <Sascha.zierke@capitalbay.de>
2021-08-20 16:50:40 +0200

    rework sliders with full JS

--- main.js
+++ main.js
@@ -14,14 +14,31 @@ function showElement(){
         }))
 }
 
-i=0
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function changeHeadline(){
-    headlines = ['Supercore', 'Core/Core Plus' , 'Value Added', 'Opportunity', 'Development', 'Workout']
-    el = document.getElementById('hsheader')
-    el.innerHTML = headlines[i]
-    i++
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-    if(i == headlines.length)
-        i = 0
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
