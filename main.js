c1971324d40ad61d47964ffb2bb11fbef27399b8
CBSaschaZierke <Sascha.zierke@capitalbay.de>
2021-08-23 12:29:48 +0200

    add sliding for footer add home.html, fonts.html and commercial.html

--- main.js
+++ main.js
@@ -19,12 +19,17 @@ let slideIndex = 0;
 function autoShowSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++){
+      dots[i].className = dots[i].className.replace(" active", "");
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
   setTimeout(autoShowSlides, 10000);
 }
 
